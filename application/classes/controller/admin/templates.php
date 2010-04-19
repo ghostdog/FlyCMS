6f48d6e285d78e532ff6b522f90db546a99a0162
unknown <=>
2010-04-19 18:11:10 +0200

    praca z js

--- application/classes/controller/admin/templates.php
+++ application/classes/controller/admin/templates.php
@@ -7,6 +7,8 @@ class Controller_Admin_Templates extends Controller_Admin_Admin {
         $this->msg_key = 'templates';
         $this->set_page_title('Szablony');
         $this->load_page_content('templates');
+        $this->set_content_var('templates', $this->model->get_templates());
+        $this->set_content_var('template', $this->model);
     }
 
     public function action_index() {
@@ -14,7 +16,6 @@ class Controller_Admin_Templates extends Controller_Admin_Admin {
     }
 
     public function action_add() {
-       $this->set_page_content('template', $this->model);
        if ($_POST) {
            $this->model->values($_POST);
            $is_valid = $this->model->validate_template(arr::merge($_POST, $_FILES));
